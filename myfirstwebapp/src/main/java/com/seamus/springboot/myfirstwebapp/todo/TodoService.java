c1916cf241252f373ffe16e07acdb7247ba7507a
stynan27 <seamustynan16@gmail.com>
2023-02-06 18:31:15 -0500

    Delete Todo

--- myfirstwebapp/src/main/java/com/seamus/springboot/myfirstwebapp/todo/TodoService.java
+++ myfirstwebapp/src/main/java/com/seamus/springboot/myfirstwebapp/todo/TodoService.java
@@ -3,6 +3,7 @@ package com.seamus.springboot.myfirstwebapp.todo;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import org.springframework.stereotype.Service;
 
@@ -30,5 +31,13 @@ public class TodoService {
     public void addTodo(String username, String description, LocalDate targetDate, boolean done) {
         Todo todo = new Todo(++todosCount, username, description, targetDate, done);
         todos.add(todo);
+    }
+
+    public void deleteById(int id) {
+        // if condition matches, remove specific Todo
+        Predicate<? super Todo> predicate
+            // bean name -> for every bean check id
+            = todo -> todo.getId() == id;
+        todos.removeIf(predicate);
     }
 }
